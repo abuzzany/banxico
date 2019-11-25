69512fae2c99571e9812324d9acc3378b6509324
Angel Buzany <abuzany@resuelve.mx>
2019-11-24 21:51:10 -0600

    Inicializa api_url y token

--- lib/banxico.rb
+++ lib/banxico.rb
@@ -1,13 +1,19 @@
-# Third party gems
+# Thirdparty gems
 require "jsonclient"
+
+# Classes
+require "banxico/series/exchange_rate"
+require "banxico/api"
+require "banxico/result"
 
 # Version
 require "banxico/version"
 
 module Banxico
+  @api_url = "https://www.banxico.org.mx/SieAPIRest/service/v1/"
+  @token = nil
   class Error < StandardError; end
   class << self
-    @api_url = nil
 
     attr_accessor :token,
                   :api_url
