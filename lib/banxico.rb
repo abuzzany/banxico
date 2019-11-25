6eb262e397efa5ef6efa939b9867227993c72d82
Angel Buzany <abuzany@resuelve.mx>
2019-11-24 19:47:58 -0600

    Agrega clase para manejar token y api_ulr

--- lib/banxico.rb
+++ lib/banxico.rb
@@ -1,6 +1,15 @@
+# Third party gems
+require "jsonclient"
+
+# Version
 require "banxico/version"
 
 module Banxico
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    @api_url = nil
+
+    attr_accessor :token,
+                  :api_url
+  end
 end
